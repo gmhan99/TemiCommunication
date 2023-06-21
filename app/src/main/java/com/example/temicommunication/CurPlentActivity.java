2af2f9999baff09ce073bd5c0ddb591872931125
AnDongH <87707867+AnDongH@users.noreply.github.com>
2023-06-21 20:47:26 +0900

    water update

--- app/src/main/java/com/example/temicommunication/CurPlentActivity.java
+++ app/src/main/java/com/example/temicommunication/CurPlentActivity.java
@@ -4,9 +4,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class CurPlentActivity extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +39,28 @@ public class CurPlentActivity extends AppCompatActivity {
         Button waterBtn2 = (Button)findViewById(R.id.btn3);
         Button waterBtn3 = (Button)findViewById(R.id.btn4);
 
+        DatabaseReference wRef1 = firebaseDatabase.getReference("number4").child("data");
 
+        waterBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wRef1.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        int flag = (int) snapshot.getValue(Integer.class);
+                        if (flag == 0)
+                            wRef1.setValue(1);
+                        else if (flag == 1)
+                            wRef1.setValue(0);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
 
     }
 }
